2de5424388d73994503b3d94d1322af475ab50c8
taggatron <tagg_daniel@hotmail.com>
2025-08-05 09:55:35 +0100

    updated for github oages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+// Set base for GitHub Pages deployment
 export default defineConfig({
+  base: '/Impact-GPE/',
   plugins: [react()],
-  preview: {
-    allowedHosts: ['impact-gpe.onrender.com'],
-  },
 })
+
